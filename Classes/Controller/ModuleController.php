c194be3e004ab8a3ac06308449ac15d26cedb3f3
Raphael Graf <r@undefined.ch>
2022-09-12 19:29:07 +0200

    Fix whitespace, newlines and  license

--- Classes/Controller/ModuleController.php
+++ Classes/Controller/ModuleController.php
@@ -1,30 +1,18 @@
 <?php
-
 declare(strict_types=1);
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2016-2021 Raphael Graf <r@undefined.ch>
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  All rights reserved
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 namespace Undefined\TranslateLocallang\Controller;
 
@@ -94,7 +82,7 @@ class ModuleController extends ActionController
      * @return ResponseInterface
      */
     public function listAction(string $extension = '', string $file = '', array $langKeys = ['default'], bool $sort = FALSE, array $overrideLabels = []): ResponseInterface
-    { 
+    {
         $moduledata = TranslateUtility::getModuleData();
         $sessid = $GLOBALS['BE_USER']->getSession()->getIdentifier();
         if (!empty($moduledata) && $extension !== '0' ) {
