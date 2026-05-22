df78da178ffc5953005e1f8c6b0c8425b5028243
Raphael Graf <r@undefined.ch>
2026-05-22 14:43:35 +0200

    Drop support for Typo3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'undefined',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-14.3.99',
+            'typo3' => '14.3.0-14.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
